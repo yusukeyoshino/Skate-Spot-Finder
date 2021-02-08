d77afc3f419550ef6bdbb407c1f9cac86a198987
Yusuke Yoshino <y.yoshino1011@gmail.com>
2021-02-08 11:56:20 -0800

    sytle spotsListView for responsive

---
+++ src/components/SpotsFilter/SpotsFilter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./SpotsFilter.module.css";
+
+const SpotsFilter = () => {
+  return <div className={classes.wrapper}></div>;
+};
+
+export default SpotsFilter;
